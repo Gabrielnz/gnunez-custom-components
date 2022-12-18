7a4755ef4f0df5985332d2471140f2de31dcc08d
Gabriel Nuñez <gabriel.nzn@gmail.com>
2022-12-18 16:50:54 +0000

    added app.vue

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <hello-vuetify />
+</template>
+
+<script lang="ts">
+import HelloVuetify from './components/HelloVuetify.vue';
+
+export default {
+    components: {
+        HelloVuetify
+    }
+}
+</script>
