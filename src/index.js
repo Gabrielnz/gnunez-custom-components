1e1a77be7ec0467382728b958d8373f0545a9959
Gabriel Nuñez <gabriel.nzn@gmail.com>
2022-12-18 18:29:10 +0000

    removing global register

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import HelloVuetify from '@/components/HelloVuetify.vue'
 import HelloTailwind from '@/components/HelloTailwind.vue'
-import Vue from 'vue'
 
 const Components = {
     HelloVuetify,
     HelloTailwind
 }
-
-Object.keys(Components).forEach(name => {
-    Vue.component(name, Components[name])
-})
 
 export default Components
